1b91d6e30e19f2d1c1eaa2f44a05733da638973a
James Olsen <olsenjames1116@gmail.com>
2023-02-10 10:50:22 -0700

    Test that caesarCipher shifts lowercase strings backwards

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -8,18 +8,26 @@ test('Converts a string to an array and back to a string', () => {
     expect(typeof caesarCipher('word')).toBe('string');
 });
 
-test('Returns a ciphered lowercase string shifted by one letter', () => {
+test('Returns a ciphered lowercase string shifted forwards by one letter', () => {
     expect(caesarCipher('abcd', 1)).toBe('bcde');
 });
 
-test('Returns a ciphered uppercase string shifted by one letter', () => {
+test('Returns a ciphered uppercase string shifted forwards by one letter', () => {
     expect(caesarCipher('ABCD', 1)).toBe('BCDE');
 });
 
-test('Returns a ciphered lowercase string shifted by multiple letters', () => {
+test('Returns a ciphered lowercase string shifted forwards by multiple letters', () => {
     expect(caesarCipher('abcd', 5)).toBe('fghi');
 });
 
-test('Returns a ciphered uppercase string shifted by multiple letters', () => {
+test('Returns a ciphered uppercase string shifted forwards by multiple letters', () => {
     expect(caesarCipher('ABCD', 5)).toBe('FGHI');
+});
+
+test('Returns a ciphered lowercase string shifted backwards by one letter', () => {
+    expect(caesarCipher('wxyz', -1)).toBe('vwxy');
+});
+
+test('Returns a ciphered lowercase string shifted backwards by multiple letters', () => {
+    expect(caesarCipher('wxyz', -3)).toBe('tuvw');
 });
